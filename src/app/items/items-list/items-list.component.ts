d00c2d51a5a568de1c4e01dcdfcbb8d29da09e9f
Fransjo Leihitu <info@coreelements.nl>
2018-12-18 15:26:21 +0100

    ItemsListComponent

--- src/app/items/items-list/items-list.component.ts
+++ src/app/items/items-list/items-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject,throwError, of , BehaviorSubject} from 'rxjs';
+
+import { ItemsService } from '../_services/items.service';
+import { ItemModel } from '../_models/item.model';
 
 @Component({
   selector: 'app-items-list',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsListComponent implements OnInit {
 
-  constructor() { }
+  items$: BehaviorSubject<ItemModel[]>;
+
+  constructor(
+    private itemsService: ItemsService
+  ) { }
 
   ngOnInit() {
+    this.items$  = this.itemsService.items$;
   }
 
 }
