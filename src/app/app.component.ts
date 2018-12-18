92c73496daea6f66dfc1eb701a2be2466d87a688
Fransjo Leihitu <info@coreelements.nl>
2018-12-18 15:37:15 +0100

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItemsService } from './items/_services/items.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud-example';
 
   navToggle: Boolean = false;
+
+
+  constructor(
+    private itemsService: ItemsService,
+  ) { }
+
+  ngOnInit() {
+    this.itemsService.fetch().subscribe();
+  }
 
   toggleNav() {
     this.navToggle  = !this.navToggle;
