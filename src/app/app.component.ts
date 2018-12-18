b24b4269c037bab6bed9fd8964e9efe43fae87f2
Fransjo Leihitu <info@coreelements.nl>
2018-12-18 15:11:29 +0100

    adding basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud-example';
+
+  navToggle: Boolean = false;
+
+  toggleNav() {
+    this.navToggle  = !this.navToggle;
+  }
 }
